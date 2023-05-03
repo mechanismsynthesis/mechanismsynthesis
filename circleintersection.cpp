f6c3a6f6e9c4a3c745652fe09612bc4a89f38c7d
Abhinav Gangula <gangulaabhinav@gmail.com>
2023-05-03 19:06:04 +0530

    Add optional to return when solution is no found

--- circleintersection.cpp
+++ circleintersection.cpp
@@ -1,7 +1,13 @@
 #include <Eigen/Dense>
 #include <iostream>
+#include <optional>
 
 #include "circleintersection.h"
+
+struct TwoCircles
+{
+    double x1 = 0.0, y1 = 0.0, r1 = 0.0, x2 = 0.0, y2 = 0.0, r2 = 0.0;
+};
 
 // Circle intersection equations
 // (x1 - x)^2 + (y1 - y)^2 = r1^2
@@ -15,12 +21,12 @@
 //      (-2x2 + 2x) (-2y2 + 2y)]
 // Deltax (aks dx) = -Jinv * F
 // Increment x = x + dx where f(x) is below the threshold
-Eigen::Vector2d GetCircleIntersection(double x1, double y1, double r1, double x2, double y2, double r2)
+std::optional<Eigen::Vector2d> GetCircleIntersection(double x1, double y1, double r1, double x2, double y2, double r2)
 {
     const double minError = 0.0001;
     const int maxIterations = 100;
     Eigen::Matrix2d J; J << 0.0, 0.0, 0.0, 0.0; // Jacobian
-    Eigen::Vector2d xy(1.11, 2.22); // initial guess
+    Eigen::Vector2d xy(1.11, 2.22); // initial guess, midpoint of two 
     Eigen::Vector2d dxy(0.0, 0.0); // delta x and y
     Eigen::Vector2d fxy(0.0, 0.0); // function
     double error = 1.0;
@@ -34,20 +40,40 @@ Eigen::Vector2d GetCircleIntersection(double x1, double y1, double r1, double x2
         error = abs(fxy.sum());
         J << -2*x1+2*x, -2*y1+2*y, -2*x2+2*x, -2*y2+2*y;
         dxy = -J.inverse()*fxy;
-        std::cout << "Iteration:" << iteration << " Error: " << error << std::endl;
         ++iteration;
     }
 
-    return xy;
+    std::cout << "Iterations: " << iteration << " Error: " << error << std::endl;
+
+    if (error <= minError)
+    {
+        return xy;
+    }
+    else
+    {
+        return std::nullopt;
+    }
 }
 
 // Prints intersection point from x1, y1, r1 defining first circle, x2, y2, r2 defining second circle
 void CircleIntersection()
 {
-    double x1 = 1.0, y1 = 2.0, r1 = 3.0, x2 = 2.0, y2 = 1.0, r2 = 3.0;
-    auto intersection = GetCircleIntersection(x1, y1, r1, x2, y2, r2);
-    std::cout << "Intersection of circles:" << std::endl;
-    std::cout << "x1: " << x1 << " y1: " << y1 << " r1: " << r1 << std::endl;
-    std::cout << "x2: " << x2 << " y2: " << y2 << " r2: " << r2 << std::endl;
-    std::cout << "is x: " << intersection[0] << " y: " << intersection[1] << std::endl;
+    std::vector<TwoCircles> circlePairs{
+        {1.0, 2.0, 3.0, 2.0, 0.0, 3.0},
+        {1.0, 2.0, 3.0, 10.0, 15.0, 3.0},
+        {10.0, 250.0, 335.0, 58.0, 92.0, 400.0},
+    };
+    for (auto c: circlePairs)
+    {
+        auto intersection = GetCircleIntersection(c.x1, c.y1, c.r1, c.x2, c.y2, c.r2);
+        std::cout << "Intersection of circles:" << "x1: " << c.x1 << " y1: " << c.y1 << " r1: " << c.r1 << "x2: " << c.x2 << " y2: " << c.y2 << " r2: " << c.r2 ;
+        if (intersection)
+        {
+            std::cout << " is x: " << (*intersection)[0] << " y: " << (*intersection)[1] << std::endl;
+        }
+        else
+        {
+            std::cout << " Not found" << std::endl;
+        }   
+    }
 }
