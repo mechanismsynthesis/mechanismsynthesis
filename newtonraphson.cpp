99e91ac060b4755c4d6dc6876bc5c9806c4d786a
Abhinav Gangula <gangulaabhinav@gmail.com>
2023-05-07 02:21:31 +0530

    Spell checks

--- newtonraphson.cpp
+++ newtonraphson.cpp
@@ -7,12 +7,12 @@ namespace NewtonRaphson {
     if (this->iterations != other.iterations)
         return false;
 
-    // Compare for options return vectors. 
-    if (this->vector == std::nullopt)
-        return other.vector == std::nullopt;
+    // Compare for optional vector. If oen if optional, both should be optional
+    if (this->vector == std::nullopt || other.vector == std::nullopt)
+        return other.vector == std::nullopt && other.vector == std::nullopt;
 
     int p = std::pow(10, -precision);
-    // Only compare error if vector is not optionsl. Else errer might vary a lot on paltforms (aks Butterfly effect).
+    // Only compare error if vector is not optional. Else error might vary a lot on paltforms (aka Butterfly effect).
     if (std::abs(this->error - other.error) <= p)
         return false;
 
@@ -28,7 +28,8 @@ namespace NewtonRaphson {
     return true;
  }
 
-
+// Newton Raphson method of finding solution to a non-liner set of equations
+// Wiki - https://en.wikipedia.org/wiki/Newton%27s_method
 // F is the Function, Eg:
 // f = [(x1 - x)^2 + (y1 - y)^2 - r1^2
 //      (x2 - x)^2 + (y2 - y)^2 = r2^2]
@@ -42,7 +43,7 @@ Solution GetSolution(Eigen::VectorXd xy, Interface &interface, const Options &op
     int size = xy.size();
     Eigen::MatrixXd J(size, size); // Jacobian
     Eigen::VectorXd fxy(size); // function
-    double error = options.minError + 1.0; // initial error should be greter than minError
+    double error = options.minError + 1.0; // initial error should be greater than minError
     int iteration = 0;
     while (error > options.minError && iteration < options.maxIterations)
     {
